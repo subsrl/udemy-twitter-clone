0e8eec5f6f1a401b47c083b041d9e2e582b9780b
subsrl <subsri84@gmail.com>
2021-09-16 10:04:35 +0530

    Started the xcode project udemy-twitter-clone.

---
+++ udemy-twitter-clone/Shared/udemy_twitter_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  udemy_twitter_cloneApp.swift
+//  Shared
+//
+//  Created by Srinivas on 16/09/21.
+//
+
+import SwiftUI
+
+@main
+struct udemy_twitter_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
